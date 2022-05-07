9cb8dc101056b97e9f3c608ac3825b7751e6b1b3
Sarah Gerrard <98355961+gerrardsarah@users.noreply.github.com>
2022-05-07 12:54:28 -0700

    made a task list repo - removed from a general folder

---
+++ HTML, CSS, JS Practice - ToDoList/README.md
@@ -0,0 +1,24 @@
+# Welcome to my Task List!
+
+A simple task list created using only HTML, CSS and JavaScript.
+
+
+https://user-images.githubusercontent.com/98355961/163289824-61098300-1bcf-466f-aadb-76b56a64e292.mp4
+
+
+## Table of Contents
+
+* [General Info](#general-info)
+* [Technologies](#technologies).
+
+## General Info
+
+With the knowledge gained within my coding journey thus far, I decided to combine all the information I've acquired into a simple project to best showcase it. Simple HTML was utilized to create the content and allow for user inputs and interactions. 
+
+With the CSS, I attempted my first utilization of Frameworks. Rather than using the popular options such as Bootstrap or Tailwind, I used [Open Props](https://open-props.style/). In addition to Open Props, I used [Glassmorphism CSS Generator](https://ui.glass/generator/) to create the look I was going for.
+
+As for the JavaScript, I adapted the video from (this youtuber)[https://www.youtube.com/watch?v=ykuD2QOZkhc] to and added a few of my own touches to it to have the desired effect.
+
+## Technologies
+
+Project was created using HTML5, CSS3, and ES13.
---
+++ HTML, CSS, JS Practice - ToDoList/js/main.js
@@ -0,0 +1,100 @@
+
+const inputBox = document.querySelector('.inputField input');
+const  addTask = document.querySelector('.inputField button');
+const thingsToDo = document.querySelector('.toDoList');
+const deleteEverything = document.querySelector('.footer button');
+
+
+//Button Transparency
+
+inputBox.onkeyup = () => {
+    let userData = inputBox.value;
+    if (userData.length > 0) {
+        addTask.classList.add('active');
+    } else {
+        addTask.classList.remove('active');
+    }
+}
+
+// When Button is clicked
+
+addTask.onclick = () => {
+    let getLocalStorage = localStorage.getItem('New Task');
+    if (getLocalStorage == null) {
+        listArray = [];
+    } else{
+        listArray = JSON.parse(getLocalStorage);
+    }
+    listArray.push(inputBox.value);
+    localStorage.setItem('New Task', JSON.stringify(listArray));
+    showTask();
+    addTask.classList.remove('active');
+}
+
+// On click, add tasks written in input to the list in the dom
+
+function showTask(){
+    let getLocalStorage = localStorage.getItem('New Task');
+    if (getLocalStorage == null) {
+        listArray = [];
+    } else{
+        listArray = JSON.parse(getLocalStorage);
+    }
+
+    const taskNumber = document.querySelector('.taskNumber');
+    taskNumber.innerHTML = listArray.length;
+
+    if (listArray.length > 0) {
+        deleteEverything.classList.add('active');
+    } else {
+        deleteEverything.classList.remove('active');
+        addTask.classList.remove('active')
+    }
+
+
+    let newInput = '';
+    listArray.forEach((element, index) => {
+        newInput += `<li> ${element} <span onclick=deleteTask(${index}); class="material-icons">delete_outline</span></li>`;
+    });
+    thingsToDo.innerHTML = newInput;
+    inputBox.value = '';
+}
+
+// With pressing the trash can, delete remove tasks from list
+
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem('New Task');
+    listArray = JSON.parse(getLocalStorage);
+    listArray.splice(index, 1);
+
+    localStorage.setItem('New Task', JSON.stringify(listArray));
+    showTask();
+}
+
+
+// Remove tasks from list when clicked
+
+deleteEverything.onclick = () => {
+    listArray = [];
+    localStorage.clear();
+    showTask();
+}
+
+// // upon pressing the checkbox, the task is completed
+
+// function completedTask(){
+    
+// }
+
+// // upon pressing the edit button, the task is editable
+
+// function editTask(){
+    
+// }
+
+// // upon pressing the save or enter button, the task is saved
+
+// // update pending tasks when a tasks is added or removed
+
+
+
